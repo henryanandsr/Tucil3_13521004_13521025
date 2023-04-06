1da28f196ed3a17f1f6c97944b32e2e1108c8987
Haidar <16521522@mahasiswa.itb.ac.id>
2023-04-07 02:10:19 +0700

    Adding adjacent class

---
+++ Adjacent.py
@@ -0,0 +1,21 @@
+import math
+
+class AdjacentB:
+    def __init__(self, start, adjacent):
+        # node
+        self.start = start
+        # list of nodes
+        self.adjacent = adjacent
+    
+    def getDistance(self, node):
+        for adjacentIndex in range(len(self.adjacent)):
+            if self.adjacent[adjacentIndex].name == node.name:
+                return math.sqrt((self.start.X - self.adjacent[adjacentIndex].X)**2 + (self.start.Y - self.adjacent[adjacentIndex].Y)**2)
+    
+class Adjacent:
+    def __init__(self, start, adjacent):
+        # node
+        self.start = start
+        # list of nodes with weight/distance
+        self.adjacent = adjacent
+    
