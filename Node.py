9f099135235c51c9ff55885eddcfd017ae48f2b9
Haidar <16521522@mahasiswa.itb.ac.id>
2023-04-07 02:11:10 +0700

    Adding Node class

---
+++ Node.py
@@ -0,0 +1,18 @@
+import math
+
+class NodeB:
+    def __init__(self,X, Y, name):
+        self.X = X
+        self.Y = Y
+        self.name = name
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+
+
+    
+
+    
+    
+    
