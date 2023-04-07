0e7518562342779b98069f34e7ec389756bf66fe
henryanandsr <13521004@std.stei.itb.ac.id>
2023-04-07 10:09:59 +0700

    update : readfile

--- main.py
+++ main.py
@@ -10,18 +10,32 @@ def read(file):
 
     array_of_nodes = []
     array_of_adjacent = []
+    arrOfNode = []
     i = 0
-    for line in lines:
+    for j in range (len(lines)):
+        num = int(lines[0])
+        if j > num :
+            break
+        if (j != 0):
+            arrOfNode.append(n.Node(lines[j]))
+    
+    for j in range (num+1,len(lines)):
         adjacent = []
-        temp = n.Node(i)
-        numbers = line.strip().split(' ')
+        temp = arrOfNode[i]
+        numbers = lines[j].strip().split(' ')
         for node in range(len(numbers)):
             if numbers[node] != '0':
-                adjacent.append([n.Node(int(node)), numbers[node]])
-
+                adjacent.append([arrOfNode[node], numbers[node]])
         array_of_nodes.append(temp)
         array_of_adjacent.append(a.Adjacent(temp, adjacent))
         i += 1
-    
+
     return array_of_adjacent, array_of_nodes
         
+# a,b =(read("./test/Test.txt"))
+# # print(a)
+# for i in range (len(a)):
+#     a[i].display()
+
+# for i in range (len(b)):
+#     b[i].display()
