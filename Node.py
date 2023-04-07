074960e4820be0d32a292e3a48d9332f5ae65b2e
Haidar <16521522@mahasiswa.itb.ac.id>
2023-04-07 14:24:42 +0700

    edit atribute for node.py (make tracking easier)

--- Node.py
+++ Node.py
@@ -7,10 +7,16 @@ class NodeB:
         self.name = name
 
 class Node:
-    def __init__(self, name):
+
+    def __init__(self, name, previous = None):
         self.name = name
+        self.previous = previous
+
     def display(self):
         print(self.name)
+    
+    def setPrev(self, previous):
+        self.previous = previous
 
 
     
