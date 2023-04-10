cf64d2265c38f98018f9a911e12edc6cf2f7c1f3
Haidar <16521522@mahasiswa.itb.ac.id>
2023-04-10 20:38:50 +0700

    edit Node.py

--- Node.py
+++ Node.py
@@ -1,13 +1,22 @@
 import math
 
 class NodeB:
-    def __init__(self,name):
+    def __init__(self,name, previous = None):
         self.name = name
+        self.previous = previous
+        
     def setCoor(self,X,Y):
         self.X = X
         self.Y = Y
+
     def display(self):
         print(self.name)
+
+    def setPrev(self, previous):
+        self.previous = previous
+
+    def getDistance(self, node):
+        return math.sqrt((self.X - node.X)**2 + (self.Y - node.Y)**2)
 
 class Node:
 
