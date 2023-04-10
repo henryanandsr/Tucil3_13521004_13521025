1e968f73957cfd8512a2d2e55754d64d9f437d61
Haidar <16521522@mahasiswa.itb.ac.id>
2023-04-10 20:40:12 +0700

    edit main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def UCS(start, end, adjacents):
                 temp_node = temp_node.previous
             path.append(start.name)
             path = path[::-1]
-            print(visited)
+            # print(visited)
             return current[0], path
         
         if current[1].name not in visited:
@@ -104,15 +104,69 @@ def UCS(start, end, adjacents):
             
 
     return None
+
+
+def UCS_B(start, end, adjacents):
+
+    # pqueue = queue.PriorityQueue()
+    
+    weight_matrix = []
+    for i in range(len(adjacents)):
+        weight = []
+        for j in range(len(adjacents[i].adjacent)):
+            weight.append(adjacents[i].start.getDistance(adjacents[i].adjacent[j][0]))
+
+        weight_matrix.append(weight)
+
+    pqueue = []
+    visited = []
+    # pqueue.put([0, start])
+    pqueue.append([0, start])
+    path = []
+
+    while pqueue:
+        min_val = min(pqueue) 
+        min_idx = pqueue.index(min_val)  
+        current = pqueue.pop(min_idx) 
+        if current[1].name == end.name:
+            visited.append(current[1].name)
+            temp_node = current[1]
+            while(temp_node != start):
+                path.append(temp_node.name)
+                temp_node = temp_node.previous
+            path.append(start.name)
+            path = path[::-1]
+            print(visited)
+            return current[0], path
         
+        if current[1].name not in visited:
+            temp_node = current[1]
+            visited.append(current[1].name)
+            for i in range(len(adjacents)):
+                if adjacents[i].start.name == current[1].name:
+                    for j in range(len(adjacents[i].adjacent)):
+                        if adjacents[i].adjacent[j][0].name not in visited:
+                            adjacents[i].adjacent[j][0].setPrev(current[1])
+                            # pqueue.put([int(current[0]) + adjacents[i].adjacent[j][1], adjacents[i].adjacent[j][0]])
+                            pqueue.append([int(current[0]) + weight_matrix[i][j], adjacents[i].adjacent[j][0]])
+                            
+        else:   
+            continue
+            
+
+    return None
+
 
 # def main ():
-    # adjacents, nodes = read("./test/Test.txt")
-    # print(UCS(nodes[3], nodes[0], adjacents))
-    # adjacents, nodes = readWithCoor("./test/buahbatu.txt")
-    # for el in adjacents:
-    #     el.display()
-    # for el in nodes:
-    #     el.display()
+#     adjacents, nodes = read("./test/Test.txt")
+#     # print(UCS(nodes[3], nodes[0], adjacents))
+#     adjacents, nodes = readWithCoor("./test/buahbatu.txt")
+#     # print(len(adjacents[0].adjacent))
+#     print(UCS_B(nodes[3], nodes[6], adjacents))
+#     # print("sate")
+#     for el in adjacents:
+#         el.display()
+#     for el in nodes:
+#         el.display()
 # main()
     
